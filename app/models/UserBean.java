d990d81f609379e3d55e8f6c2c7cee6d650e2f65
Summerlve <dzxx0563zh@126.com>
2016-02-14 10:03:49 +0000

    fack den

--- app/models/UserBean.java
+++ app/models/UserBean.java
@@ -1,9 +1,22 @@
 package models;
 
 import com.avaje.ebean.Model;
+import javax.persistence.*;
+
 
 /**
  * Created by Summer on 2016/2/11.
  */
+@Entity
+@Table(name = "User")
 public class UserBean extends Model {
+    @Id
+    @Column(name="id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name="name", length = 255, nullable = false, unique = true, )
+    public String name;
+
+
 }
